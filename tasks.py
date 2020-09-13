0a764b7cf7f2b820bc9b3deaa6e9abf923096650
Allen Liu <alex03108861@gmail.com>
2020-09-13 12:55:18 +0800

    Add config file

--- tasks.py
+++ tasks.py
@@ -2,6 +2,8 @@ from celery import Celery
 
 app = Celery('tasks', backend='rpc://', broker='amqp://localhost')
 
+app.config_from_object('celeryconfig')
+
 @app.task
 def add(x, y):
     return x + y
